8ad4b20342bcc58d01151590a0bc63a788cd41b3
virgilio1974 <vitrgilio1974@gmail.com>
2021-12-29 18:20:08 +0100

    Adding first draft for typespecs

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,9 +1,20 @@
 defmodule Hangman do
-  def new_game do
 
+  @type game :: any
+  @type tally :: %{
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(String.t)
+  }
+  @type state :: :initialising | :won | :lost | :good_guess | :bad_guess | :already_used
+
+  @spec new_game() :: game
+  def new_game do
   end
 
-  def make_move(game, guess) do
+  @spec make_move(game, String.t) :: { game, tally }
+  def make_move(_game, _guess) do
 
   end
 end
