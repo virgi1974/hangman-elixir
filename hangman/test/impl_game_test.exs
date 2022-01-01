7704397c5992aef9351f84aa2be93ed4e54291b6
virgilio1974 <vitrgilio1974@gmail.com>
2022-01-01 09:08:42 +0100

    Handling guess letters

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -28,9 +28,31 @@ defmodule HangmanImplGameTest do
   test "state doesn't change if a game is won or lost" do
     for state <- [:won, :lost] do
       game = Game.new_game("wombat")
-      game = Map.put(game, :game_state, :won)
+      game = Map.put(game, :game_state, state)
       { new_game, _tally } = Game.make_move(game, "x")
       assert new_game == game
     end
+  end
+
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "record letters used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "b")
+    assert MapSet.equal?(game.used, MapSet.new(["b"]))
+    # stored only once
+    {game, _tally} = Game.make_move(game, "b")
+    assert MapSet.equal?(game.used, MapSet.new(["b"]))
+    # stored in sorted way
+    {game, _tally} = Game.make_move(game, "a")
+    assert MapSet.equal?(game.used, MapSet.new(["a", "b"]))
   end
 end
